a561e8dcc4734186b817eca1277ade6af937e7b2
JokerXin <jokerxin@126.com>
2025-07-08 22:40:59 +0800

    Add files via upload

---
+++ model/_Derivative.h
@@ -0,0 +1,24 @@
+/// 2025 Z·Calculator.Derivative
+
+# pragma once
+
+# include "trans.h"
+
+// Type External Declaration
+
+extern object_type Any_Type;
+extern object_type Variable;
+extern object_type Real;
+extern object_type Parser_Primer;
+extern function_type Root;
+
+extern function_type * function_list[];
+extern operation_type * operation_list[];
+
+// .Derivative
+
+extern void * * * * E_utmost_sequence;
+extern void * * * * * D_single_sequence;
+
+void Derivative_initializer(void);
+void Derivative_calculator(node *);
