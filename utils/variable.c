7db21ce689ed19a4b3788fd8daef241fe0ed8689
JokerXin <jokerxin@126.com>
2025-07-09 13:09:38 +0800

    Update variable.c

--- utils/variable.c
+++ utils/variable.c
@@ -8,11 +8,14 @@ variable * * variable_list_initializer(variable * * list_){
 
     // initialize the variable list
 
+    // [PARAMETER] [variable list]
+
     if(list_){
-        int n = -1;
-        while(list_[++n]){
+        size_t n = 0;
+        while(list_[n]){
             free(list_[n]->symbol);
             free(list_[n]);
+            n++;
         }
         free(list_);
     }
@@ -24,11 +27,13 @@ variable * * variable_list_initializer(variable * * list_){
 
 variable * * registerer(node * const node_, const char * const symbol_, variable * * list_){
 
-    // Look up the symbol in the variable list. If this symbol is
-    // 1. already registered in the variable list : renew node's value
-    // 2. not registered yet in the variable list : add it to the variable list and renew node's value
+    // look up the symbol in the variable list and if this symbol is
+    // 1. already registered in the variable list : renew object's value
+    // 2. not registered yet in the variable list : add it to the variable list and renew object's value
 
-    int n = 0;
+    // [PARAMETER] [object node], [symbol], [variable list]
+
+    size_t n = 0;
     while(list_[n]){
         if(!strcmp(list_[n]->symbol, symbol_)){
             /* case 1 */
@@ -38,9 +43,9 @@ variable * * registerer(node * const node_, const char * const symbol_, variable
         n++;
     }
     /* case 2 */
-    list_ = realloc(list_, (n+2)*sizeof(variable *));
+    list_ = realloc(list_, (n+2) * sizeof(variable *));
     list_[n] = malloc(sizeof(variable));
-    list_[n]->symbol = malloc((strlen(symbol_)+1)*sizeof(char));
+    list_[n]->symbol = malloc((strlen(symbol_)+1) * sizeof(char));
     strcpy(list_[n]->symbol, symbol_);
     list_[n+1] = NULL;
     node_->child_l = (node *)list_[n];
