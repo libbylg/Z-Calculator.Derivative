a82d9800c3bb1e50e5967fece957f7a3250ecda7
JokerXin <jokerxin@126.com>
2025-07-09 13:06:54 +0800

    Update Console.c

--- Console.c
+++ Console.c
@@ -13,11 +13,11 @@ int main(void){
     double time;
     char * input = NULL;
     tree * Main = malloc(sizeof(tree));
-    Main->root = malloc(sizeof(node));
+    Main->root = calloc(1, sizeof(node));
     Main->root->category = FUNCTION;
     Main->root->type = &Root;
     Main->root->parent = (node *)Main;
-    Main->root->child_r = NULL;
+    Main->root->child_l = NULL;
     Main->variable_list = NULL;
     Derivative_initializer();
     PRINT_CLS;
@@ -25,10 +25,9 @@ int main(void){
         print("\n>> ");
         /* Initializer */
         remove_subtree(Main->root->child_l);
-        Main->root->child_l = malloc(sizeof(node));
+        Main->root->child_l = calloc(1, sizeof(node));
         Main->root->child_l->category = OBJECT;
         Main->root->child_l->type = &Real;
-        Main->root->child_l->child_l = NULL;
         Main->root->child_l->parent = Main->root;
         Main->variable_list = variable_list_initializer(Main->variable_list);
         /* Input */
@@ -43,7 +42,7 @@ int main(void){
         /* Calculator */
         time = clock();
         Derivative_calculator(Main->root);
-        trans_utmost(Main->root->child_l, (void * *)E_utmost_sequence);
+        trans_utmost(Main->root->child_l, (const void * *)E_utmost_sequence);
         time = (clock() - time) / CLOCKS_PER_SEC;
         /* Printer */
         Print_s(Main->root);
