4c648663cbe4ebf78087e07915e3c636e0759c29
JokerXin <jokerxin@126.com>
2025-07-09 13:07:57 +0800

    Update node_type.c

--- utils/node_type.c
+++ utils/node_type.c
@@ -4,10 +4,12 @@
 
 // Node Type Utilities
 
-function_type * search_function_type(const char * const prefix_, function_type * const * const list_){
+const function_type * search_function_type(const char * const prefix_, const function_type * const * const list_){
 
     // search for the function type by its prefix
-    // [REMARK] Return NULL when the function type does not exist.
+    // [REMARK] Return NULL when the function type does not exist in the function type list.
+
+    // [PARAMETER] [prefix], [function type list]
 
     for(int i = 0; list_[i]; i++){
         if(!strcmp(list_[i]->prefix, prefix_)){
@@ -18,10 +20,12 @@ function_type * search_function_type(const char * const prefix_, function_type *
 
 }
 
-operation_type * search_operation_type(const char * const prefix_, operation_type * const * const list_){
+const operation_type * search_operation_type(const char * const prefix_, const operation_type * const * const list_){
 
     // search for the operation type by its prefix
-    // [REMARK] Return NULL when the operation type does not exist.
+    // [REMARK] Return NULL when the operation type does not exist in the operation type list.
+
+    // [PARAMETER] [prefix], [operation type list]
 
     for(int i = 0; list_[i]; i++){
         if(!strcmp(list_[i]->prefix, prefix_)){
