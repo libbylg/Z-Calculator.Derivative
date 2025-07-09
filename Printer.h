43e58cab41f9277dc0634c2fdc3ee579eda66b47
JokerXin <jokerxin@126.com>
2025-07-09 13:05:38 +0800

    Update Printer.h

--- Printer.h
+++ Printer.h
@@ -10,9 +10,9 @@
 
 // Printer Macros
 
-# define PRINT_RESET    //printf("\033[0m")
-# define PRINT_BOLD     //printf("\033[1m")
-# define PRINT_CLS      //printf("\033[2J\033[H")
+# define PRINT_RESET    printf("\033[0m")
+# define PRINT_BOLD     printf("\033[1m")
+# define PRINT_CLS      printf("\033[2J\033[H")
 
 // Printer Utility
 
